c1a0082ce1867cf2d10d0b168cae0115620c7655
hont <hont@gmail.com>
2022-06-28 17:34:10 +0700

    reflection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DependencyInjection;
 use App\Http\Controllers\UserController;
+use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +21,39 @@ Route::get('/', function () {
 });
 
 Route::get('/users/register', [UserController::class, 'register']);
+
+Route::get('/dependency-injection', [DependencyInjection::class, 'register']);
+/*
+* ReflectionClass ky thuat khoi tao dependency injection cua laravel
+* Skill check xem 1 route can dependency nhung gi de chay
+* co the debug cac class can gi 
+* day cx la ky thuat khoi tao class cua laravel Reflection Class
+*/
+Route::get('reflection', function () {
+    $reflection = new ReflectionClass(UserController::class);
+    $parameters = $reflection->getConstructor()->getParameters();
+
+    $method = $reflection->getMethod('register');
+    //dd($method);
+    // dd($parameters[0]->getClass());
+
+    $container = Container::getInstance();
+
+    $dependency = [];
+    foreach ($parameters as $parameter) {
+        $dependency[] = $container->make($parameter->getType()->getName());
+    }
+    // dd($dependency);
+
+    $controller = new UserController(...$dependency);
+    dd($controller);
+    $needToInjected = [];
+    foreach ($parameters as $parameter) {
+        $parameter->getName();
+        $parameter->getClass();
+    }
+
+});
 
 // Route::get('/{any}', function () {
 //     return view('post');
